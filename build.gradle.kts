e351422fb87616687da6f65fc79ca541f7edda88
Victor Kuenstler <victor.kuenstler@bakdata.com>
2021-09-08 11:42:50 +0200

    Add feedback

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,5 @@ dependencies {
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
     testImplementation(group = "org.assertj", name = "assertj-core", version = "3.20.2")
-    testImplementation(group = "log4j", name = "log4j", version = "1.2.17")
     testImplementation(group = "com.bakdata.fluent-kafka-streams-tests", name = "fluent-kafka-streams-tests-junit5", version = "2.3.1")
 }
